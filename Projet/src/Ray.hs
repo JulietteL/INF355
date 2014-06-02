774ec1a05c5dd1f5fbed1396c01f6934e6cb8b1e
JulietteL <juliette.lagrange@sfr.fr>
2014-06-02 17:26:42 +0200

    Add intersection Ray/Plan

--- Projet/src/Ray.hs
+++ Projet/src/Ray.hs
@@ -16,5 +16,18 @@ intersect (Ray o d) (Sphere ce r) = let
                                    in if (delta <  0) then Nothing else let
     t1 = (-b + sqrt delta)/a
     t2 = (-b - sqrt delta)/a
-    in if (t1 < t2 && t1 > 0) then Just (o + (mul t1 d), o) else if (t2 < t1 && t2 > 0) then Just(o + (mul t2 d), o) else Nothing
+    in if (t1 <= t2 && t1 > 0) then
+         let p1 = o + (mul t1 d)
+         in Just (p1, p1 - ce)
+       else if (t2 < t1 && t2 > 0) then
+              let p2 = o + (mul t2 d)
+              in Just(p2, p2 - ce)
+            else Nothing
 
+
+intersect' :: Ray -> Plan -> Maybe (Vec3Df, Vec3Df)
+intersect' (Ray o d) (Plan p n) =
+  if (dot d n == 0) then Nothing
+  else let t = (1/(dot d n)) * (dot (p-o) n)
+       in if (t < 0) then Nothing
+          else Just(o + mul t d, n )
