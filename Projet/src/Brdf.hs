2ec187128f87edc3fe9b4d0bf22b6e6dad32fc5b
kazmiero <alex.kazmierowski@gmail.com>
2014-06-27 14:48:01 +0200

    Add scene examples, comment Brdf.hs and add global parameters

--- Projet/src/Brdf.hs
+++ Projet/src/Brdf.hs
@@ -8,18 +8,41 @@ import Scene
 import Ray
 import Vector
 
+-- global parameters
+
+-- Background Color
 bgColor :: Vec3Df
 bgColor = (Vec3Df 10 10 10)
 
-sqDistanceToCam :: Vec3Df -> (Maybe (Vec3Df,Vec3Df), Material) -> Float
-sqDistanceToCam cam (Just (point,normal), _) = squaredNorm $ point - cam
+-- Activate shadows
+shadows :: Bool
+shadows = False
 
+-- Number of rays casted to extended lights (soft shadow)
+shadowRays :: Int
+shadowRays = 16
+
+-- Maximum number of rebounds for mirrors
+maxRebounds :: Int
+maxRebounds = 5
+-------------------------
+
+-- function used to sort the intersections 
+sqDistanceToCam :: Vec3Df -> (Maybe (Vec3Df,Vec3Df), Material) -> Float
+sqDistanceToCam cam (Just (point,_), _) = squaredNorm $ point - cam
+
+-- Apply the ray tracing algorithm to a set of antialiasing rays
+-- Parameters : scene objects, scene lights, rays, brdf type, list of random numbers
+-- Returns : (brdf value, tail of the random numbers' list)
 brdfs :: [Object] -> [Light] -> [Ray] -> String -> [Float] -> (Vec3Df, [Float])
-brdfs objs lights [] brdfName rlist = (0, rlist)
+brdfs _ _ [] _ rlist = (0, rlist)
 brdfs objs lights (ray:t) brdfName rlist = let (c,rlist') = brdf objs lights ray brdfName 0 rlist
                                                (c',rlist'') = brdfs objs lights t brdfName rlist'
                                              in (c+c', rlist'') 
 
+-- Apply the ray tracing algorithm to a single ray
+-- Parameters : scene objects, scene lights, ray, brdf type, mirror iteration, list of random numbers
+-- Returns : (brdf value, tail of the random numbers' list)
 brdf :: [Object] -> [Light] -> Ray -> String -> Int -> [Float] -> (Vec3Df, [Float])
 brdf objs lights (Ray o d) brdfName i rList= let intList = filter (\x -> isJust $ fst x) [(intersect (Ray o d) obj, getMaterial(obj)) | obj <- objs ]
                       in if null intList
@@ -29,22 +52,29 @@ brdf objs lights (Ray o d) brdfName i rList= let intList = filter (\x -> isJust 
                            (brdf1, rList') = brdf' ((p,n), mat) lights o brdfName objs rList
                               in if getReflexiveCoeff mat == 0
                                  then (brdf1, rList') 
-                                 else if i < 5
+                                 else if i < maxRebounds
                                       then let (brdf2, rList'') = brdf objs lights (reflectedRay (Ray o d) (p,n)) brdfName (i+1) rList'
                                            in (mul (getReflexiveCoeff mat) brdf2 + mul (1 - getReflexiveCoeff mat) brdf1, rList'')
                                       else (brdf1, rList')
-                                           
+
+
+-- Calculate the shadow coefficient on a point by casting a ray to the light
+-- Parameters : position, light, scene objects
+-- Return : 1 if a scene object is intersected, else 0
 shadowCoeff :: Vec3Df -> Light -> [Object] -> Float
 shadowCoeff pos (ExtendedLight lp lc _) objs = shadowCoeff pos (Light lp lc) objs
-shadowCoeff pos (Light lp lc) objs = let dir = normalize $ lp - pos
+shadowCoeff pos (Light lp _) objs = let dir = normalize $ lp - pos
                                      in let ray = Ray (pos +  (mul 0.01 dir)) dir
                                       in let intList = filter (\x -> isJust x && squaredNorm (fst (fromJust x) - pos) < squaredNorm (lp - pos)) [intersect ray obj | obj <- objs]
                                              
                                          in if null intList
                                             then 1.0
                                             else 0.0
-                                                 
--- brdf : (pt intersection, normale au pt d'intersection, matériau), lumières, position de la caméra -> Couleur 
+
+-- Apply the Bidirectional Reflectance Distribution Function at an intersected object
+-- Parameters : intersectionPoint : ((position,normal), object material), scene lights, camera position, brdf type, scene objects, list of random numbers
+-- Returns : (color, tail of the random numbers' list)
+-- brdf types are "lambert" and "phong" 
 brdf' :: ((Vec3Df, Vec3Df), Material) -> [Light] -> Vec3Df -> String -> [Object] -> [Float] -> (Vec3Df, [Float])
 -- Lambert
 brdf' ((p, n), mat) lights _ "lambert" _ random = (sum $ fmap (\(Light lp lc) -> mul (max 0 $ dot (normalize (lp-p)) n) ( lc * getDiffuseColor mat)) lights, random)
@@ -58,23 +88,30 @@ brdf' u (light:otherLights) cam "phong" objs random =
 brdf' _ _ _ "phong"_ _ = error "object's material is not of type SpecularMaterial"
 brdf' _ _ _ _ _ _ = error "no such BRDF"
 
+
+-- Apply the BRDF component of one light
+-- Parameters : intersectionPoint ((position,normal), object material), light, camera position, brdf type, scene objects, list of random numbers
+-- Returns : (color, tail of the random numbers' list)
 brdf'' :: ((Vec3Df, Vec3Df), Material) -> Light -> Vec3Df -> String -> [Object] -> [Float] -> (Vec3Df, [Float])
 brdf'' ((p, n), (SpecularMaterial d kd s ks sh)) (ExtendedLight lp lc r) cam "phong" objs random =
-  let nbPoints = 16
-      h = normalize $ (normalize (lp - p)) + (normalize (cam - p))
-      (lights', random') = getPointsOnLight (ExtendedLight lp lc r) nbPoints random
-      sc = (sum $ fmap (\x -> shadowCoeff p x objs) lights')/(fromIntegral nbPoints)
+  let h = normalize $ (normalize (lp - p)) + (normalize (cam - p))
+      (lights', random') = getPointsOnLight (ExtendedLight lp lc r) shadowRays random
+      sc = if shadows
+           then (sum $ fmap (\x -> shadowCoeff p x objs) lights')/(fromIntegral shadowRays)
+           else 1
   in (mul sc $ ((mul (kd * (max 0 $ dot (normalize $ lp-p) n)) d)
                + mul (ks * (max 0 $ dot h n) ** sh) s) * lc, random')
 brdf'' ((p, n), (SpecularMaterial d kd s ks sh)) (Light lp lc) cam "phong" objs random =
   let h = normalize $ (normalize (lp - p)) + (normalize (cam - p))
-      sc = shadowCoeff p (Light lp lc) objs
+      sc = if shadows
+           then shadowCoeff p (Light lp lc) objs
+           else 1 
   in (mul sc $ ((mul (kd * (max 0 $ dot (normalize $ lp-p) n)) d)
                + mul (ks * (max 0 $ dot h n) ** sh) s) * lc, random)
 -- reflexive
-brdf'' ((p, n), (ReflexiveMaterial f d kd s ks sh)) light cam "phong" objs random =
+brdf'' ((p, n), (ReflexiveMaterial _ d kd s ks sh)) light cam "phong" objs random =
   brdf'' ((p, n), (SpecularMaterial d kd s ks sh)) light cam "phong" objs random
 
 
--- WARNING : should not exist
+-- WARNING : debug used to find non handled cases
 brdf'' _ _ _ _ _ rList = (Vec3Df 0.0 0.0 255.0, rList)
--- Projet/src/SceneExamples.hs
+++ Projet/src/SceneExamples.hs
@@ -32,9 +32,13 @@ createScene2 :: Scene
 createScene2 = let cam = Camera (Vec3Df 0 0 (-10)) (Vec3Df 0 0 0)
                    objs = [
                      Sphere (Vec3Df 0 0 0) 1 (mat1 $ Vec3Df 255 0 0),
-                     Sphere (Vec3Df 2 2 5) 3 (mat2 $ Vec3Df 30 189 64)
+                     Sphere (Vec3Df 2 2 5) 3 (mat2 $ Vec3Df 30 189 64),
+                     Sphere (Vec3Df (-2) (-2) 3) 1.5 (mat2 $ Vec3Df 40 40 220)
                           ]
-                   lights = [ExtendedLight (Vec3Df (-50) 0 (-30)) (Vec3Df 1 1 1) 20]
+                   lights = [
+                     ExtendedLight (Vec3Df (-50) 0 (-30)) (Vec3Df 1 1 1) 3,
+                     ExtendedLight (Vec3Df 50 0 (-30)) (Vec3Df 0.4 0 0.6) 2
+                     ]
                   in (cam, objs, lights)
 
 createScene3 :: Scene
@@ -45,4 +49,35 @@ createScene3 = let cam = Camera (Vec3Df 3 0 (-10)) (Vec3Df 0 0 0)
                      Plan (Vec3Df (-1) 0 0) (Vec3Df 1 0 0) (mat3 0.8 $ Vec3Df 0 255 0)
                           ]
                    lights = [Light (Vec3Df 0 5 (-4)) (Vec3Df 1 1 1)]
+                  in (cam, objs, lights)
+
+createScene4 :: Scene
+createScene4 = let cam = Camera (Vec3Df 0 0 (-10)) (Vec3Df 0 0 0)
+                   objs = [
+                     Sphere (Vec3Df 0 0 0) 1 (mat1 $ Vec3Df 255 0 0),
+                     Sphere (Vec3Df 2 2 5) 3 (mat3 0.8 $ Vec3Df 15 15 115),
+                     Sphere (Vec3Df (-2) (-2) 3) 1.5 (mat2 $ Vec3Df 40 40 220),
+                     Sphere (Vec3Df (-1) 1 2) 1.2 (mat2 $ Vec3Df 255 230 255),
+                     Plan (Vec3Df 0 0 10) (Vec3Df 0 0.5 (-1)) (mat3 0.95 $ Vec3Df 255 255 255)
+                     ]
+                   lights = [
+                     ExtendedLight (Vec3Df (-50) 0 (-30)) (Vec3Df 0.8 0.2 0) 3,
+                     ExtendedLight (Vec3Df 50 0 (-30)) (Vec3Df 0.5 0.8 1) 2
+                     ]
+                  in (cam, objs, lights)
+                  
+createScene5 :: Scene
+createScene5 = let cam = Camera (Vec3Df 0 0 (-10)) (Vec3Df 0 0 0)
+                   r = 1.0
+                   objs = [
+                     Sphere (Vec3Df (-r) 0 0) r (mat3 0.6 $ Vec3Df 64 224 208),
+                     Sphere (Vec3Df r 0 0) r (mat3 0.6 $ Vec3Df 50 205 50 ),
+                     Sphere (Vec3Df 0 (-r*(sqrt 3)) 0) r (mat3 0.6 $ Vec3Df 154 130 205) ,
+                     Plan (Vec3Df 0 r 0) (Vec3Df 0 (-1) 0 ) (mat3 0.85 $ Vec3Df 255 240 240),
+                     Plan (Vec3Df (-2*r) 0 0) (Vec3Df 1 0 0 ) (mat3 0.85 $ Vec3Df 255 240 240),
+                     Plan (Vec3Df (2*r) 0 0) (Vec3Df (-1) 0 0 ) (mat3 0.85 $ Vec3Df 255 240 240),
+                     Plan (Vec3Df 0 0 r) (Vec3Df 0 0 (-1)) (mat3 0.85 $ Vec3Df 255 240 240),
+                     Plan (Vec3Df 0 (-r*(1 + sqrt 3)) 0) (Vec3Df 0 1 0 ) (mat3 0.85 $ Vec3Df 255 240 240)
+                     ]
+                   lights = [Light (Vec3Df 0 0 (-30)) (Vec3Df 1 1 1) ] --ExtendedLight (Vec3Df 0 0 (-30)) (Vec3Df 1 1 1) 5 ]
                   in (cam, objs, lights)
